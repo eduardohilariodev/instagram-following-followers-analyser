75860669cc5c6d5df4fabaa152da83a747ccee88
Eduardo Hilário <eduhilario@proton.me>
2023-09-10 23:51:03 -0300

    layout: align title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import FileUploader from "./FileUploader";
 import "@fontsource/space-grotesk";
-
+import { Instagram } from "lucide-react";
 const App: React.FC = () => {
   return (
     <>
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h3></h3>
         <div className="flex h-screen flex-col items-center justify-center">
+          <h3 className="mb-6 flex items-center justify-center gap-3">
+            <Instagram className="inline" /> Instagram analyser
+          </h3>
           <div className="flex flex-row">
             <FileUploader />
           </div>
