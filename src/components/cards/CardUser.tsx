9901217e8d35093ee0257869c3f104f699bd06c2
Eduardo Hilário <eduhilario@proton.me>
2023-09-10 23:52:31 -0300

    fix: unused href

--- src/components/cards/CardUser.tsx
+++ src/components/cards/CardUser.tsx
@@ -6,7 +6,7 @@ interface CardUserProps {
   href: string;
 }
 
-const CardUser: React.FC<CardUserProps> = ({ username, date, href }) => {
+const CardUser: React.FC<CardUserProps> = ({ username, date }) => {
   return (
     <li className="overflow-hidden bg-white/10 px-4 py-4 shadow backdrop-blur-sm sm:rounded-md sm:px-6">
       <h3 className="text-base font-semibold leading-6 text-slate-300">
